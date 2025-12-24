56fdd49857c8b1e32105ece26a03ec5e9d251aab
pradhun-krishna <pradhunkrishna123@google.com>
2025-12-24 20:16:19 +0530

    update1

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-base: '/https://github.com/pradhun-krishna/nilgiris-fresh-connect.git/'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/nilgiris-fresh-connect/',
   server: {
     host: "::",
     port: 8080,
