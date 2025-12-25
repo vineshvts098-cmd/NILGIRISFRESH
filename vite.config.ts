061de2a2e29675e65c5d8ff157473a5104835c1a
vineshvts098-cmd <vineshvts098@gmail.com>
2025-12-25 11:42:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/nilgiris-fresh-connect/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
