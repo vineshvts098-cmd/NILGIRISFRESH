e7f4f3b81531692c260893001ff3bdb0070e0f9f
vineshvts098-cmd <vineshvts098@gmail.com>
2025-12-25 11:49:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/', // Netlify deploys to root by default
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    outDir: 'dist', // Netlify expects build output in 'dist'
   },
   plugins: [react()],
   resolve: {
